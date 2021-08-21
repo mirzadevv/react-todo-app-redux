a6b3fb52f57d823559e997cdc7c09f6ec4cb9daa
mirzaei.dev <mirzaei.devv@gmail.com>
2021-08-22 01:31:51 +0430

    feat: use todos state
    By using useSelector, we can choose the specific state (todos)

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,28 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
 const TodosList = () => {
+  const todos = useSelector((state) => state.todos);
   return (
     <div className="list-wrapper">
       <ul className="d-flex flex-column todo-list">
-        <li>
-          <div className="form-check">
-            <label className="form-check-label">
-              <input className="checkbox" type="checkbox" />
-              Buy Milk.
-              <i className="input-helper"> </i>
-            </label>
-          </div>
-          <i className="remove mdi mdi-close-circle-outline"></i>
-        </li>
-        <li className="completed">
-          <div className="form-check">
-            <label className="form-check-label">
-              <input className="checkbox" type="checkbox" checked="" />
-              Wash the dishes.
-              <i className="input-helper"></i>
-            </label>
-          </div>
-          <i className="remove mdi mdi-close-circle-outline"></i>
-        </li>
+        {todos.length === 0 ? (
+          <h5> There are no todos</h5>
+        ) : (
+          <>
+            {todos.map((todo) => (
+              <li key={todo.id} className={todo.isCompleted ? "completed" : ""}>
+                <div className="form-check">
+                  <label className="form-check-label">
+                    <input className="checkbox" type="checkbox" />
+                    {todo.title}
+                    <i className="input-helper"> </i>
+                  </label>
+                </div>
+                <i className="remove mdi mdi-close-circle-outline"></i>
+              </li>
+            ))}
+          </>
+        )}
       </ul>
     </div>
   );
