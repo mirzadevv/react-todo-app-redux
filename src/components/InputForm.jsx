050dddd4c93ea0e696fc1eeb3fcdf90dfd4807b5
mirzaei.dev <mirzaei.devv@gmail.com>
2021-08-22 01:15:11 +0430

    feat: add handleAddTodo
    In InputForm I've used store.dispatch for sending an object to action parameter in todosReducer via the store

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import store from "../store";
+
 const InputForm = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleAddTodo = () => {
+    store.dispatch({
+      type: "add",
+      payload: { id: uuid(), title: inputValue, isCompleted: false },
+    });
+    setInputValue("");
+  };
+
   return (
     <div className="add-items d-flex">
       <input
         type="text"
         className="form-control todo-list-input"
         placeholder="What do you need to do today?"
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
       />
-      <button className="add btn btn-primary font-weight-bold todo-list-add-btn">
+      <button
+        onClick={handleAddTodo}
+        className="add btn btn-primary font-weight-bold todo-list-add-btn"
+      >
         Add
       </button>
     </div>
